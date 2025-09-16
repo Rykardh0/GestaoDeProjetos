f95bb96b2eb63ab35fc9c955deec75f1d0e6fa15
Ricardo Henrique de Souza Santana <ricardosantana@fanese.edu.br>
2025-09-16 11:29:13 -0300

    questao2e3

--- EXERCICIO.js
+++ EXERCICIO.js
@@ -1,2 +1,14 @@
+// Questão Numero 1
 let saudacao="Olá, JavaScript";
 console.log(saudacao);
+
+//Questão Numero 2
+var idade = 30 ; // tem escopo de função ou global, e pode ser reatribuído e redeclarado.
+let nome = "Ricardo"; // tem escopo de bloco e pode ser reatribuído, mas não redeclarado.
+const PI = 3.14; // em escopo de bloco e não pode ser nem reatribuído nem redeclarado, exigindo inicialização. 
+
+//Questão Numero 3
+let cidade = "Socorro" // Criação da Variavel cidade com a atribuição do nome Socorro.
+console.log(cidade);
+cidade = "Nova Cidade"; // Alteração do nome Socorro para Nova Cidade.
+console.log(cidade);
